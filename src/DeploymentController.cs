53e29dcc5391e85bb32f61c540429f8359477bb9
101584958 <101584958.swin.edu.au>
2019-09-20 17:24:09 +1000

    fix ship placement and direction

--- src/DeploymentController.cs
+++ src/DeploymentController.cs
@@ -67,7 +67,7 @@ static class DeploymentController
                 GameController.EndDeployment();
             else if (UtilityFunctions.IsMouseInRectangle(UP_DOWN_BUTTON_LEFT, TOP_BUTTONS_TOP, DIR_BUTTONS_WIDTH,
                 TOP_BUTTONS_HEIGHT))
-                _currentDirection = Direction.LeftRight;
+                _currentDirection = Direction.UpDown;
             else if (UtilityFunctions.IsMouseInRectangle(LEFT_RIGHT_BUTTON_LEFT, TOP_BUTTONS_TOP, DIR_BUTTONS_WIDTH,
                 TOP_BUTTONS_HEIGHT))
                 _currentDirection = Direction.LeftRight;
@@ -93,9 +93,8 @@ static class DeploymentController
 
         // Calculate the row/col clicked
         int row, col;
-        row = Convert.ToInt32(Math.Floor((mouse.Y) / (UtilityFunctions.CELL_HEIGHT + UtilityFunctions.CELL_GAP)));
-        col = Convert.ToInt32(Math.Floor((mouse.X - UtilityFunctions.FIELD_LEFT) /
-                                         (UtilityFunctions.CELL_WIDTH + UtilityFunctions.CELL_GAP)));
+        row = Convert.ToInt32(Math.Floor((mouse.Y - UtilityFunctions.FIELD_TOP) / (UtilityFunctions.CELL_HEIGHT + UtilityFunctions.CELL_GAP)));
+        col = Convert.ToInt32(Math.Floor((mouse.X - UtilityFunctions.FIELD_LEFT) / (UtilityFunctions.CELL_WIDTH + UtilityFunctions.CELL_GAP)));
 
         if ((row >= 0) & (row < GameController.HumanPlayer.PlayerGrid.Height))
         {
@@ -133,7 +132,7 @@ static class DeploymentController
         foreach (ShipName sn in Enum.GetValues(typeof(ShipName)))
         {
             int i = default(int);
-            i = (int) sn - 1;
+            i = (int)sn - 1;
             if (i >= 0)
             {
                 if (sn == _selectedShip)
@@ -158,8 +157,8 @@ static class DeploymentController
     {
         foreach (ShipName sn in Enum.GetValues(typeof(ShipName)))
         {
-            int i = default(int);
-            i = (int) sn - 1;
+            int i = 0;
+            i = (int)sn - 1;
 
             if (UtilityFunctions.IsMouseInRectangle(SHIPS_LEFT, SHIPS_TOP + (i * SHIPS_HEIGHT), SHIPS_WIDTH,
                 SHIPS_HEIGHT))
